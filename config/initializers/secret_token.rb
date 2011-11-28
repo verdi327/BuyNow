f0e22ad50534730b82cdfc442f7cce7cdd325bcf
Michael <michael.v.verdi@gmail.com>
2011-11-28 14:01:11 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BuyNow::Application.config.secret_token = '8f8f076de4b1bde6cf75a237845dc86489f0baa3539ee928ec6dd5fad71de61de3bcb5b2d643851e2912914e6372b3d620c7999265b7871ea34ad8773f4c5d49'
